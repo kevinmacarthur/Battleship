20850be3352c4ca75eba6d35ede2c54284dc570f
kevinmacarthur <kmacarthur16@gmail.com>
2019-01-11 00:54:48 +0000

    made the game squares empty and send coordinate and player when clicked

--- client/src/Gamespace.jsx
+++ client/src/Gamespace.jsx
@@ -6,21 +6,18 @@ class Gamespace extends Component {
     super()
     this.state = {
       coordinate: props.coordinate,
-      status: props.coordinate
+      status: ""
     }
     this.handleClick= this.handleClick.bind(this)
   }
 
   handleClick(e) {
-    console.log("clicked", e.target.value)
-    console.log("e.target is", e.target)
-    console.log("bye", e.target.className)
-    console.log('cell belongs to', e.target.attributes['player'].value)
+    console.log("clicked", e.target.attributes.coordinate.value, 'belonging to', e.target.attributes.player.value)
   }
 
   render() {
     return (
-      <input player={this.props.player} className='cell' value={this.state.status} onClick={this.handleClick}></input>
+      <input player={this.props.player} coordinate={this.state.coordinate} className='cell' value={this.state.status} onClick={this.handleClick}></input>
       )
   }
 }
