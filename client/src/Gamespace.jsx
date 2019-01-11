29f0d7a9474f37c94e1567ae555f75eb159e6e02
kevinmacarthur <kmacarthur16@gmail.com>
2019-01-11 00:47:28 +0000

    added grid to be 10x10 and when clicked send coordinate and player

--- client/src/Gamespace.jsx
+++ client/src/Gamespace.jsx
@@ -1,14 +1,28 @@
 import React, {Component} from 'react';
 
-function Gamespace ({coordinate})  {
+class Gamespace extends Component {
 
-  function handleClick(e) {
-    console.log("clicked", e.target.value)
+  constructor(props) {
+    super()
+    this.state = {
+      coordinate: props.coordinate,
+      status: props.coordinate
+    }
+    this.handleClick= this.handleClick.bind(this)
   }
 
+  handleClick(e) {
+    console.log("clicked", e.target.value)
+    console.log("e.target is", e.target)
+    console.log("bye", e.target.className)
+    console.log('cell belongs to', e.target.attributes['player'].value)
+  }
+
+  render() {
     return (
-      <input value={coordinate} onClick={handleClick}></input>
+      <input player={this.props.player} className='cell' value={this.state.status} onClick={this.handleClick}></input>
       )
+  }
 }
 
 export default Gamespace;
