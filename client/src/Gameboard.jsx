28d2e9915f2bfdd975961cc612f9ee40fb2c1a82
kevinmacarthur <kmacarthur16@gmail.com>
2019-01-11 01:26:38 +0000

    made coordinates an object instead of array

--- client/src/Gameboard.jsx
+++ client/src/Gameboard.jsx
@@ -7,7 +7,108 @@ class Gameboard extends Component {
 
     this.state = {
       socket: this.props.socket,
-      coordinates: ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8', 'A9', 'A10', 'B1', 'B2', 'B3', 'B4', 'B5', 'B6', 'B7', 'B8', 'B9', 'B10', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9', 'C10', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7', 'D8', 'D9', 'D10', 'E1', 'E2', 'E3', 'E4', 'E5', 'E6', 'E7', 'E8', 'E9', 'E10', 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7', 'F8', 'F9', 'F10','G1', 'G2', 'G3', 'G4', 'G5', 'G6', 'G7', 'G8', 'G9', 'G10','H1', 'H2', 'H3', 'H4', 'H5', 'H6', 'H7', 'H8', 'H9', 'H10', 'I1', 'I2', 'I3', 'I4', 'I5', 'I6', 'I7', 'I8', 'I9', 'I10', 'J1', 'J2', 'J3', 'J4', 'J5', 'J6', 'J7', 'J8', 'J9', 'J10']
+      coordinates: {
+        A1: '',
+        A2: '',
+        A3: '',
+        A4: '',
+        A5: '',
+        A6: '',
+        A7: '',
+        A8: '',
+        A9: '',
+        A10: '',
+        B1: '',
+        B2: '',
+        B3: '',
+        B4: '',
+        B5: '',
+        B6: '',
+        B7: '',
+        B8: '',
+        B9: '',
+        B10: '',
+        C1: '',
+        C2: '',
+        C3: '',
+        C4: '',
+        C5: '',
+        C6: '',
+        C7: '',
+        C8: '',
+        C9: '',
+        C10: '',
+        D1: '',
+        D2: '',
+        D3: '',
+        D4: '',
+        D5: '',
+        D6: '',
+        D7: '',
+        D8: '',
+        D9: '',
+        D10: '',
+        E1: '',
+        E2: '',
+        E3: '',
+        E4: '',
+        E5: '',
+        E6: '',
+        E7: '',
+        E8: '',
+        E9: '',
+        E10: '',
+        F1: '',
+        F2: '',
+        F3: '',
+        F4: '',
+        F5: '',
+        F6: '',
+        F7: '',
+        F8: '',
+        F9: '',
+        F10: '',
+        G1: '',
+        G2: '',
+        G3: '',
+        G4: '',
+        G5: '',
+        G6: '',
+        G7: '',
+        G8: '',
+        G9: '',
+        G10: '',
+        H1: '',
+        H2: '',
+        H3: '',
+        H4: '',
+        H5: '',
+        H6: '',
+        H7: '',
+        H8: '',
+        H9: '',
+        H10: '',
+        I1: '',
+        I2: '',
+        I3: '',
+        I4: '',
+        I5: '',
+        I6: '',
+        I7: '',
+        I8: '',
+        I9: '',
+        I10: '',
+        J1: '',
+        J2: '',
+        J3: '',
+        J4: '',
+        J5: '',
+        J6: '',
+        J7: '',
+        J8: '',
+        J9: '',
+        J10: ''
+      }
     }
     this.renderBoard = this.renderBoard.bind(this)
   }
@@ -17,47 +118,13 @@ class Gameboard extends Component {
   }
 
   renderBoard(player) {
-    let board = this.state.coordinates.map(coord => {
+    let board = Object.keys(this.state.coordinates).map(coord => {
       return (
         <Gamespace coordinate={coord} player={player}/>
       )
     })
     return (board)
   }
-
-  // createTable1() {
-  //   let table = []
-  //   // Outer loop to create parent
-  //   for (let i = 0; i < 10; i++) {
-  //     let children = []
-  //     //Inner loop to create children
-  //     for (let j = 0; j < 10; j++) {
-  //       children.push(<td>{[`[${j + 1}]`]}</td>)
-  //     }
-  //     //Create the parent and add the children
-  //     table.push(<tr>{children}</tr>)
-  //   }
-  //   return table
-  // }
-
-  // createTable2() {
-  //   let table = []
-  //   // Outer loop to create parent (i is # of rows)
-  //   for (let i = 0; i < 10; i++) {
-  //     let children = []
-  //     //Inner loop to create children (j is # of columns)
-  //     for (let j = 0; j < 10; j++) {
-  //       let id = `${i}${j}2`
-  //       // let htmlElement = <GameSpace id={id} value={[`[${i}${j + 1}]`]}/>
-  //       let htmlElement = <td onClick={}id={id} >{[`[${i}${j + 1}]`]}</td>
-  //       children.push(htmlElement)
-  //       // children.push(id)
-  //     }
-  //     //Create the parent and add the children
-  //     table.push(<tr>{children}</tr>)
-  //   }
-  //   return table
-  // }
 
   render() {
     return(
