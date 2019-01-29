c87770b55cf574ed77e6baf30eb8a2551dd11c52
kevinmacarthur <kmacarthur16@gmail.com>
2019-01-29 21:37:00 +0000

    npm installed update immutability helper to help handling state and used it in ready function

--- client/src/Gameboard.jsx
+++ client/src/Gameboard.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Gamespace from './Gamespace.jsx';
 import PlaceShips from './PlaceShips.jsx';
+import update from 'immutability-helper';
 let coordinates = require("./board.js")
 
 class Gameboard extends Component {
@@ -17,13 +18,17 @@ class Gameboard extends Component {
                         'Submarine': '',
                         'Sneakyboat': ''
       },
-      tempCoords: []},
+        tempCoords: [],
+        ready:false
+      },
       playerTwo:{ships: {'Battleship': '',
                         'Destroyer': '',
                         'Submarine': '',
                         'Sneakyboat': ''
       },
-      tempCoords: []}
+        tempCoords: [],
+        ready:false
+     }
     }
     this.renderBoard = this.renderBoard.bind(this)
     this.attemptPlacement = this.attemptPlacement.bind(this)
@@ -55,16 +60,17 @@ class Gameboard extends Component {
         )
       } else {
         return (
-        <Gamespace coordinate={coord} player={player} socket={this.props.socket} placingShip={this.state.placingShip} attemptPlacement={this.attemptPlacement} hasShip={false}/>
-      )
+          <Gamespace coordinate={coord} player={player} socket={this.props.socket} placingShip={this.state.placingShip} attemptPlacement={this.attemptPlacement} hasShip={false}/>
+        )
       }
     })
     return (board)
   }
 
-   placingShip(ship, spaces) {
+   placingShip(ship, spaces, player) {
         let Battleship = {name: ship,
                           spaces:spaces,
+                          player: player,
                           coordinates:[]}
 
     this.setState({placingShip: Battleship}, () => {console.log("STATE NOW", this.state)})
@@ -92,7 +98,7 @@ class Gameboard extends Component {
             }
         )
     } else {
-        console.log("COULD NOT PLACE SHIP")
+        alert("COULD NOT PLACE SHIP")
     }
     this.setState({placingShip: false})
   }
@@ -169,25 +175,24 @@ class Gameboard extends Component {
 
   attemptPlacement (ship, coordinate, player) {
     if (this.notAlreadyTaken(coordinate, player)) {
-        console.log("ATTEMPT PLACEMENT", this.state[player])
         this.state[player].tempCoords.push(coordinate)
         if (ship.spaces === this.state[player].tempCoords.length) {
             console.log("READY TO PLACE ", ship.name, "at coords", this.state[player].tempCoords, 'for', player)
             this.placeShip(ship.name, this.state[player].tempCoords, player)
-            // this.setState({[player]:{ships:{Battleship:this.state[player].ships.Battleship, Destroyer:this.state[player].ships.Destroyer, Submarine:this.state[player].ships.Submarine, Sneakyboat:this.state[player].ships.Sneakyboat}}, tempCoords:[]}, () => {
-            //     console.log("coords NOW", this.state)
-            // })
         }
     } else {
-        console.log("SORRY THIS COORDINATE IS ALREADY TAKEN BY ANOTHER SHIP")
+        alert("SORRY THIS COORDINATE IS ALREADY TAKEN BY ANOTHER SHIP")
+        this.clearTemp(player)
     }
   }
 
   readyToStart(player){
     if (this.state[player].ships.Battleship && this.state[player].ships.Destroyer && this.state[player].ships.Submarine && this.state[player].ships.Sneakyboat) {
-        console.log(`${player} is ready to start`)
+        const newObj = update(this.state[player], {ready:{$set: this.state[player].ready=true}})
+        this.setState({[player]:newObj})
+        console.log("STATE IS", this.state)
     } else {
-        console.log(`${player} hasn't placed all their ships yet`)
+        alert(`${player} hasn't placed all their ships yet`)
     }
   }
 
@@ -203,16 +208,21 @@ class Gameboard extends Component {
               <div className='board'>
                 <div className ="player-one">
                     {this.renderBoard('playerOne', this.state.playerOne.ships)}
+                    <PlaceShips board={this.state.coordinates} placingShip={this.placingShip} currentlyPlacingShip={this.state.placingShip} player="playerOne"/>
+                    <div className="buttons">
                     <button onClick={() => this.readyToStart('playerOne')}> READY </button>
                     <button onClick={() => this.clearTemp('playerOne')}> Clear </button>
+                    </div>
                 </div>
                 <div className ="player-two">
                     {this.renderBoard('playerTwo', this.state.playerTwo.ships)}
+                    <PlaceShips board={this.state.coordinates} placingShip={this.placingShip} currentlyPlacingShip={this.state.placingShip} player="playerTwo"/>
+                    <div className="buttons">
                     <button onClick={() => this.readyToStart('playerTwo')}> READY </button>
                     <button onClick={() => this.clearTemp('playerTwo')}> Clear </button>
+                    </div>
                 </div>
               </div>
-              <PlaceShips board={this.state.coordinates} placingShip={this.placingShip}/>
             </div>
         )
     } else {
--- client/src/PlaceShips.jsx
+++ client/src/PlaceShips.jsx
@@ -1,13 +1,15 @@
 import React, {Component} from 'react';
 
-function PlaceShips ({placingShip, player}) {
+function PlaceShips ({placingShip, player, currentlyPlacingShip}) {
+
+  console.log("currentlyPlacingShip is ", currentlyPlacingShip)
 
   return (
     <div className='PlaceShips'>
-    <button onClick={() => placingShip('Battleship', 5)}> Place Battleship 5 </button>
-    <button onClick={() => placingShip('Destroyer', 4)}> Place Destroyer 4 </button>
-    <button onClick={() => placingShip('Submarine', 3)}> Place Submarine 3 </button>
-    <button onClick={() => placingShip('Sneakyboat', 2)}> Place Sneakyboat 2 </button>
+    <button className={currentlyPlacingShip.name === "Battleship" && currentlyPlacingShip.player == player ? "selected" : "button"} onClick={() => placingShip('Battleship', 5, player)}> Place Battleship 5 </button>
+    <button className={currentlyPlacingShip.name === "Destroyer" && currentlyPlacingShip.player == player ? "selected" : "button"} onClick={() => placingShip('Destroyer', 4, player)}> Place Destroyer 4 </button>
+    <button className={currentlyPlacingShip.name === "Submarine" && currentlyPlacingShip.player == player ? "selected" : "button"} onClick={() => placingShip('Submarine', 3, player)}> Place Submarine 3 </button>
+    <button className={currentlyPlacingShip.name === "Sneakyboat" && currentlyPlacingShip.player == player ? "selected" : "button"} onClick={() => placingShip('Sneakyboat', 2, player)}> Place Sneakyboat 2 </button>
     </div>
     )
 }
